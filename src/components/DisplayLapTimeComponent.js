ad1ccb4f426a285cffedf42e162831fad91778b5
nrc2539 <narongchai.b2539@gmail.com>
2021-01-15 19:10:59 +0700

    change laptime table

--- src/components/DisplayLapTimeComponent.js
+++ src/components/DisplayLapTimeComponent.js
@@ -4,7 +4,8 @@ import { Table } from 'antd';
 function DisplayLapTimeComponent({listLapTime}) {
 	const columns = [
 		{title: 'Lap', dataIndex: 'lap', key: 'lap'},
-		{title: 'Split Time', dataIndex:'time', key:'time', align: 'right'}
+		{title: 'Lap Time', dataIndex:'lapTimeText', key:'lapTimeText', align: 'right'},
+		{title: 'Split Time', dataIndex:'splitTimeText', key:'splitTimeText', align: 'right'}
 	]
 	const displayTable = listLapTime.length > 0 ? <Table columns={columns} dataSource={listLapTime} pagination={{ pageSize: 5, position: ['none','bottomCenter']}}></Table> : ''
 	return (
